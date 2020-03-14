ee5b492ba7308168979c2db3935ced3bfe2efa67
João Carlos Fraqueiro da Palma <joaocarlospalma.io@gmail.com>
2020-03-14 23:48:53 +0000

    Initial Build
    - Initial build with only header and profile info

---
+++ precache-manifest.d933e96511093666cc3190daec7a5ab2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6365d72d76876e0fc65b40e094237b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9817f095040935d922d",
+    "url": "/static/css/main.063a235b.chunk.css"
+  },
+  {
+    "revision": "62d9f25fcd303c3149b5",
+    "url": "/static/js/2.117dd0eb.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.117dd0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9817f095040935d922d",
+    "url": "/static/js/main.41c4ddbc.chunk.js"
+  },
+  {
+    "revision": "a6093a98ae916e4b08b8",
+    "url": "/static/js/runtime-main.44fdae21.js"
+  },
+  {
+    "revision": "ce9873acbe6815a78914f4b7d4c14d22",
+    "url": "/static/media/avatar_500.ce9873ac.png"
+  },
+  {
+    "revision": "58c3b025fb3cf232dd9623f0a54c8a7c",
+    "url": "/static/media/background_rsz.58c3b025.png"
+  }
+]);
